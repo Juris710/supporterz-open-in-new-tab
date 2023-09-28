9d76b720b7e12620a7eba3c534c741de0992ba6c
Juris710 <juris710.dev@gmail.com>
2023-09-28 19:15:45 +0900

    :technologist: Setup ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+/** @type {import("eslint").ESLint.ConfigData} */
+const config = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "ignorePatterns": ["dist"],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
+
+module.exports = config;
