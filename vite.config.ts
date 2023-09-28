3dec926191580302c644d02e902b60bf74ebc702
Juris710 <juris710.dev@gmail.com>
2023-09-28 20:59:43 +0900

    :sparkles: Setup Chrome extension with CRXJS

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,22 @@
+import { crx } from '@crxjs/vite-plugin';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [
+    tsconfigPaths(),
+    crx({
+      manifest: {
+        manifest_version: 3,
+        name: 'supporterz-open-in-new-tab',
+        version: '0.0.0',
+        content_scripts: [
+          {
+            matches: ['https://talent.supporterz.jp/*'],
+            js: ['src/content_scripts/main.ts'],
+          },
+        ],
+      },
+    }),
+  ],
 });
